6e375493fe6b537170cfbf82d302828aab2c195e
gabriela.lins <gabriela.lins@placeti.com>
2022-06-27 10:55:09 -0300

    teste.

---
+++ src/Flutuante.java
@@ -0,0 +1,11 @@
+public class Flutuante {
+    public static void main(String[] args) {
+        System.out.println("Teste com número flutuante");
+
+        double temperatura;
+        temperatura = 39.5;
+
+        System.out.println("A temperatura de Fernando é " + temperatura);
+
+    }
+}
---
+++ src/variaveis.java
@@ -0,0 +1,15 @@
+public class variaveis {
+
+    public static void main(String[] args) {
+        System.out.println("teste de variáveis");
+
+        int idade;
+        idade = 37;
+
+        System.out.println(idade);
+
+        idade = 37 - 7;
+
+        System.out.println("Fernando tem "  + idade + " anos");
+    }
+}
